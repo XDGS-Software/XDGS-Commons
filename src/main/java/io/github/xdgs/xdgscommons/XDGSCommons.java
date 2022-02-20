cbbf4021b7c8aef33d522d110f5d217dcfe34d02
GStudiosX2 <ramazanogutlu0@gmail.com>
2022-02-20 08:14:39 +0000

    Add loaded log

--- src/main/java/io/github/xdgs/xdgscommons/XDGSCommons.java
+++ src/main/java/io/github/xdgs/xdgscommons/XDGSCommons.java
@@ -2,14 +2,30 @@ package io.github.xdgs.xdgscommons;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Logger;
+
 public class XDGSCommons extends JavaPlugin {
+    private static XDGSCommons instance;
+
+    private Logger logger;
+
     @Override
     public void onEnable() {
+        double started = System.currentTimeMillis();
 
+        instance = this;
+        logger = getLogger();
+
+        logger.info(getName() + " has loaded in " + (System.currentTimeMillis() - started) + "ms.");
     }
 
     @Override
     public void onDisable() {
+        logger = null;
+        instance = null;
+    }
 
+    public static XDGSCommons getInstance() {
+        return instance;
     }
 }
