a9fdd274603831b3ca039ff3c5f9151aedfeec1a
GStudiosX2 <ramazanogutlu0@gmail.com>
2022-02-20 04:30:39 +0000

    Setup

---
+++ src/main/java/io/github/xdgs/xdgscommons/XDGSCommons.java
@@ -0,0 +1,15 @@
+package io.github.xdgs.xdgscommons;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class XDGSCommons extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
