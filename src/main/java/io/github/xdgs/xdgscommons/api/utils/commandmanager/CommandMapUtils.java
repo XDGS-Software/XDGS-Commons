d3af5b34ef509f984f6dd6501cf7fd1e98732b80
GStudiosX2 <ramazanogutlu0@gmail.com>
2022-03-01 23:59:54 +0000

    Fixed CommandMapUtils?

--- src/main/java/io/github/xdgs/xdgscommons/api/utils/commandmanager/CommandMapUtils.java
+++ src/main/java/io/github/xdgs/xdgscommons/api/utils/commandmanager/CommandMapUtils.java
@@ -3,28 +3,36 @@ package io.github.xdgs.xdgscommons.api.utils.commandmanager;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandMap;
+import org.bukkit.command.SimpleCommandMap;
 
 import java.lang.reflect.Field;
 import java.util.HashMap;
 
+@SuppressWarnings("unchecked")
 public class CommandMapUtils {
-    private static CommandMap commandMap;
+    private static SimpleCommandMap commandMap;
     private static HashMap<String, Command> knownCommands;
 
     static {
         try {
             Field commandMapField = Bukkit.getServer().getClass().getDeclaredField("commandMap");
             commandMapField.setAccessible(true);
-            commandMap = (CommandMap) commandMapField.get(Bukkit.getServer());
-            knownCommands =
-                    (HashMap<String, Command>) commandMap.getClass().getDeclaredField("knownCommands").get(Bukkit.getServer());
+            commandMap = (SimpleCommandMap) commandMapField.get(Bukkit.getServer());
+
+            Field knownCommandsField;
+            try {
+                knownCommandsField = commandMap.getClass().getDeclaredField("knownCommands");
+            } catch (NoSuchFieldException e) {
+                knownCommandsField = commandMap.getClass().getSuperclass().getDeclaredField("knownCommands");
+            }
+            knownCommandsField.setAccessible(true);
+            knownCommands = (HashMap<String, Command>) knownCommandsField.get(Bukkit.getServer());
         } catch (NoSuchFieldException | IllegalAccessException e) {
             e.printStackTrace();
         }
     }
 
     public static boolean registerCommand(AbstractCommand command) {
-        knownCommands.put(command.getID(), command);
         return commandMap.register(command.getID(), command);
     }
 
