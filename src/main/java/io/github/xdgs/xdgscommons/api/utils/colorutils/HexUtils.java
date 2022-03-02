0a0ed91da66ccc4882168f88ed492c4da57b0841
GStudiosX2 <ramazanogutlu0@gmail.com>
2022-03-02 17:12:20 +0000

    Create HexUtils.java

---
+++ src/main/java/io/github/xdgs/xdgscommons/api/utils/colorutils/HexUtils.java
@@ -0,0 +1,28 @@
+package io.github.xdgs.xdgscommons.api.utils.colorutils;
+
+public class HexUtils {
+    public static String nToHex(int n) {
+        return "#" + Integer.toHexString(n);
+    }
+
+    public static String rgbToHex(int r, int g, int b) {
+        return nToHex(r) +
+                (nToHex(g).replace("#", "")) +
+                (nToHex(b).replace("#", ""));
+    }
+
+    public static int hexToInt(String hex) {
+        return Integer.valueOf(hex, 16);
+    }
+
+    public static int[] hexToRGB(String hex) {
+        hex = hex.replace("#", "");
+        String rs = hex.substring(0, 2);
+        String gs = hex.substring(2, 4);
+        String bs = hex.substring(4, 6);
+        int r = hexToInt(rs);
+        int g = hexToInt(gs);
+        int b = hexToInt(bs);
+        return new int[]{r, g, b};
+    }
+}
