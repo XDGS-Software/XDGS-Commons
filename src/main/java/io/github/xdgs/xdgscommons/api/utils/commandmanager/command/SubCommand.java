d548e6a99a463642a37208a930a8b5652567e920
GStudiosX2 <ramazanogutlu0@gmail.com>
2022-03-07 21:47:19 +0000

    SubComanded commands

---
+++ src/main/java/io/github/xdgs/xdgscommons/api/utils/commandmanager/command/SubCommand.java
@@ -0,0 +1,5 @@
+package io.github.xdgs.xdgscommons.api.utils.commandmanager.command;
+
+public interface SubCommand {
+    boolean run();
+}
---
+++ src/main/java/io/github/xdgs/xdgscommons/api/utils/commandmanager/command/SubCommandedCommand.java
@@ -0,0 +1,42 @@
+package io.github.xdgs.xdgscommons.api.utils.commandmanager.command;
+
+import io.github.xdgs.xdgscommons.api.utils.commandmanager.AbstractCommand;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+import java.util.List;
+
+public abstract class SubCommandedCommand extends AbstractCommand {
+    public HashMap<String, SubCommand> subCommandHashMap = new HashMap<>();
+
+    public SubCommandedCommand(JavaPlugin plugin, String name) {
+        super(plugin, name);
+    }
+
+    @Override
+    public boolean Execute(CommandSender commandSender, String command, String[] args) {
+        if (args.length > 0) {
+            if (subCommandHashMap.containsKey(args[0])) {
+                return subCommandHashMap.get(args[0]).run();
+            }
+        }
+        return true;
+    }
+
+    public void putSubCommand(String subCommandName, SubCommand subCommand) {
+        subCommandHashMap.put(subCommandName, subCommand);
+    }
+
+    public SubCommand removeSubCommand(String subCommandName) {
+        return subCommandHashMap.remove(subCommandName);
+    }
+
+    @Override
+    public List<String> onTabComplete(@NotNull CommandSender sender, Command command, @NotNull String alias, @NotNull String[] args) {
+        if (subCommandHashMap.keySet().size() > 0) return List.of(subCommandHashMap.keySet().toArray(String[]::new));
+        return null;
+    }
+}
